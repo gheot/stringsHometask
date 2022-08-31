2e034855b9a4b2955c9296b47250d1c98eb866a2
nilova <marina.nilova@yandex.ru>
2022-08-31 22:49:35 +0300

    Homework is done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("ФИО сотрудника - " + fullName);
 
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+
+        fullName = "Иванов Семён Семёнович";
+        System.out.println("Данные ФИО сотрудника — " + fullName.replace('ё', 'е'));
     }
 }
